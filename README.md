b48b23df83a5824e31b9eb29cf1337de8f85bc12
Philip Persson <philip.persson@netset.se>
2019-03-09 15:03:10 +0100

    Docker compose configuration

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MuchAboutNothing
 Much about nothing in order to learn microservices.
+
+# Create backup from mongodb container
+docker run --rm -it --network <NETWORK> --link <CONTAINER>:<MOMGO_ALIAS> -v /backup:/backup mongo mongodump --host <MOMGO_ALIAS> --out /backup/
+
+# Restore backup from mongodb container
+docker run --rm -it --network <NETWORK> --link <CONTAINER>:<MOMGO_ALIAS> -v /backup:/backup mongo mongorestore --drop --host <MOMGO_ALIAS> /backup/
+
--- UserService/server.js
+++ UserService/server.js
@@ -3,11 +3,29 @@ const app = require('express')()
 const port = 1337
 
 app.get('/', (req, res) => {
-    mongodb.connect('mongodb://127.0.0.1:27017/users', (err, db) => {
-        res.send('Hello World!')
-    })
-})
+    mongodb.connect('mongodb://mongodb:27017', (err, db) => {
+        var dbo = db.db('prutt_db');
+ /*       dbo.collection('prutt').insertOne({lukt: 'Stinky', power: 123}, function (err, v) {
+          if (err) {
+            throw err;
+          }
+
+          db.close();
+          res.send('INSERTED PRUTT');
+        });
+*/
+        dbo.collection('prutt').find({}).toArray((err, a) => {
+          if (err) {
+            throw err;
+          }
+
+
+
+          res.send(a);
+        });
+    });
+});
 
 app.listen(port, () => {
-  console.log('App listening on port ' + port)
-})
+  console.log('App listening on port ' + port);
+});
