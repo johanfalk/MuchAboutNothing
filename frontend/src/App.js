32a965ccb9acfa436b54b98e91c58748347594bc
Philip Persson <philipp.dev@hotmail.com>
2019-03-10 20:36:15 +0100

    Login page with redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,59 +4,24 @@ import 'bulma/css/bulma.css'
 import DeckList from './components/decklist';
 import Login from './components/login';
 import Modal from 'react-responsive-modal';
+import { BrowserRouter, Route } from 'react-router-dom'
 
 class App extends Component {
 
   state = {
-    loginOpen: false,
-  };
 
-  openLoginModal = () => {
-    this.setState({ loginOpen: true });
-  };
-
-  closeLoginModal = () => {
-    this.setState({ loginOpen: false });
   };
 
   render() {
-    const { loginOpen } = this.state;
     return (
       <div className="App">
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-                    <a className="navbar-item" href="/">
-                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="" />
-                    </a>
-
-                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" href="/">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div id="navbarBasicExample" className="navbar-menu">
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <div className="buttons">
-                                <a className="button is-primary" href="/register">
-                                    <strong>Sign up</strong>
-                                </a>
-                                <button className="button is-light" onClick={this.openLoginModal}>
-                                    Log in
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        <div>
-          <Modal open={loginOpen} onClose={this.closeLoginModal} center>
-            <Login />
-          </Modal>
-          <DeckList/>
-        </div>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        <BrowserRouter>
+          <div>
+            <Route path="/login" component={Login} />
+            <Route path="/decklist" component={DeckList} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,33 +1,71 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router'
 
 class Login extends Component {
-    state = { 
-        isLoading: false
+    state = {
+        isLoading: false,
+        isLoggedIn: false,
+        email: '',
+        password: ''
     };
 
-    onLoginClick = () => {
-        this.setState({isLoading: true});
+    constructor() {
+        super();
+        //TODO check if logged in
+    }
+
+    handleEmailChange = event => {
+        this.setState({ email: event.target.value });
     };
 
-    render() { 
-        const { isLoading } = this.state;
-        const buttonClass = "button is-primary" + (isLoading ? " is-loading" : "");
-        return ( 
-        <div>
-            <h2>Login</h2>
-            <div className="field is-horizontal">
-                <div className="control">
-                    <input type="text" className="input is-medium" placeholder="email"/>
+    handlePasswordChange = event => {
+        this.setState({ password: event.target.value });
+    };
+
+    onSubmit = event => {
+        console.log(this.state);
+        event.preventDefault();
+        this.setState({ isLoggedIn: true });
+    }
+
+    render() {
+        if (this.state.isLoggedIn) {
+            return <Redirect to="/decklist" />
+        }
+
+        const buttonClass = "button is-primary" + (this.state.isLoading ? " is-loading" : "");
+        return (
+            <div className="columns">
+                <div className="column"></div>
+                <div className="column">
+                    <form onSubmit={this.onSubmit}>
+                        <label className="field">Login</label>
+                        <div className="field">
+                            <p className="control has-icons-left has-icons-right">
+                                <input className="input" type="email" placeholder="Email" ref={this.state.email} onChange={this.handleEmailChange} />
+                                <span className="icon is-small is-left">
+                                    <i className="fa fa-envelope"></i>
+                                </span>
+                                <span className="icon is-small is-right">
+                                    <i className="fa fa-check"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <div className="field">
+                            <p className="control has-icons-left">
+                                <input className="input" type="password" placeholder="Password" ref={this.state.password} onChange={this.handlePasswordChange} />
+                                <span className="icon is-small is-left">
+                                    <i className="fa fa-lock"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <input type="submit" value="Login" className={buttonClass} />
+                    </form>
                 </div>
+                <div className="column"></div>
             </div>
-            <div className="field">
-                <div className="control">
-                    <input type="password" className="input is-medium" placeholder="password"/>
-                </div>
-            </div>
-            <button onClick={this.onLoginClick} className={buttonClass} >Login</button>
-        </div> );
+        );
     }
 }
- 
+
 export default Login;
